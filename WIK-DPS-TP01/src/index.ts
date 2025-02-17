a2f6082b6d369392cdc82a400edd3c02ac797095
Gwuill <guill.branche@gmail.com>
2025-02-17 15:46:07 +0100

    TP03

--- WIK-DPS-TP01/src/index.ts
+++ WIK-DPS-TP01/src/index.ts
@@ -5,7 +5,9 @@ const port = process.env.PING_LISTEN_PORT || 3000;
 const server = http.createServer((req, res) => {
     if (req.url === '/ping' && req.method == 'GET') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(JSON.stringify(req.headers));
+        // Add the hostname of the server to the response`Pong from ${process.env.HOSTNAME}
+        //res.end(JSON.stringify(req.headers) + `{process.env.HOSTNAME}` );
+        res.end(`Pong from ${process.env.HOSTNAME}`);
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('');
